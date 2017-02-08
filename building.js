25c4acc7f1aee352faad4decc85bcb430dc98bd4
Denis <carriere.denis@gmail.com>
2017-02-08 13:36:26 -0500

    Clearly define user inputs

--- building.js
+++ building.js
@@ -6,13 +6,15 @@ const users = global.mapOptions.users
 
 module.exports = (data, tile, writeData, done) => {
   const features = []
+  const timestamp = Date.now() / 1000 - days * 24 * 60 * 60
+
   for (const feature of data.qatiles.osm.features) {
     if (feature.geometry.type === 'Polygon') {
-      // Filter by Date
-      if (!(Number(feature.properties['@timestamp']) > Date.now() / 1000 - days * 24 * 60 * 60)) { continue }
-
       // Only find buildings
       if (!feature.properties.building) { continue }
+
+      // Filter by Date
+      if (days > 0) { if (!(Number(feature.properties['@timestamp']) > timestamp)) { continue } }
 
       // Filter by Usernames
       if (users.length) { if (users.indexOf(feature.properties['@user']) === -1) { continue } }
--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const tileReduce = require('tile-reduce')
 const path = require('path')
-const turf = require('@turf/turf')
-const write = require('write-json-file')
 const load = require('load-json-file')
+const write = require('write-json-file')
+const turf = require('@turf/turf')
 
-const city = 'Ottawa'
-const days = 90
+// Input variables
+const extent = './extents/Gatineau.geojson'
+const qatiles = 'canada.mbtiles'
+const days = 120
 const users = []
-const results = turf.featureCollection([])
-const edits = {}
-const extent = load.sync(`./extents/${city}.geojson`)
 
-tileReduce({
-  geojson: extent,
+// Results
+const edits = {}
+const geojson = load.sync(extent)
+const results = turf.featureCollection([])
+
+const options = {
+  geojson,
   zoom: 12,
   map: path.join(__dirname, 'building.js'),
-  sources: [{name: 'qatiles', mbtiles: 'canada.mbtiles'}],
+  sources: [{name: 'qatiles', mbtiles: qatiles}],
   mapOptions: {
     days,
-    extent,
+    extent: geojson,
     users
   }
-})
+}
+
+tileReduce(options)
 .on('reduce', (features) => {
   features.map(feature => {
     results.features.push(feature)
-
     // Store User information
     const user = feature.properties['@user']
     if (edits[user] === undefined) {
@@ -36,8 +41,9 @@ tileReduce({
   })
 })
 .on('end', () => {
-  console.log('Extent: ' + city)
-  console.log('Past days: 60')
+  console.log('Extent:', extent)
+  console.log('Past days:', days)
+  console.log('QA Tiles:', qatiles)
   console.log('Buildings total: %d', results.features.length)
   console.log('Users:')
   const sorted = Object.keys(edits).sort((a, b) => edits[a] - edits[b]).reverse()
